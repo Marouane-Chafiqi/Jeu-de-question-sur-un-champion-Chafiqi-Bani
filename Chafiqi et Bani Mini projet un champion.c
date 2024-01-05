32c6371feee0ff090c499cd8efd0e740d74a9d33
Marouane Chafiqi <155460466+Marouane-Chafiqi@users.noreply.github.com>
2024-01-05 16:43:59 +0100

    Add files via upload

---
+++ Chafiqi et Bani Mini projet un champion.c
@@ -0,0 +1,169 @@
+
+#include <stdio.h>
+
+int main() {
+    int score = 0;
+    char answer;
+
+    printf("Bienvenue au quiz sur Cristiano Ronaldo!\n");
+
+    printf("Question 1: Quel est le pays d'origine de Cristiano Ronaldo?\n");
+    printf("a) Espagne\nb) Portugal\nc) Bresil\nd) Italie\n");
+    printf("Votre reponse : ");
+    scanf(" %c", &answer);
+
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect. La bonne reponse est le Portugal.\n");
+    }
+
+
+
+
+
+     printf("Question 2 :a quelle annees ronaldo est ne:\n");
+    printf("a) 1986 \nb) 1985\nc) 1987\nd) 1990\n");
+    printf("Votre reponse : ");
+    scanf(" %c", &answer);
+
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect. La bonne reponse est: 1985\n");
+    }
+
+
+
+
+
+
+
+
+
+
+    printf("Question 3: En quelle année Ronaldo a-t-il remporté son premier Ballon d'Or?\n");
+    printf("a) 2008\nb) 2010\nc) 2006\nd) 2012\n");
+    printf("Votre reponse : ");
+    scanf(" %c",&answer);
+
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect. La bonne reponse est 2008.\n");
+    }
+
+   
+
+
+
+
+
+printf("Question 4: Combien de ballon d'or a gagner le long de sa carriere:\n");
+    printf("a) 1\nb) 7\nc) 5\nd) 4\n");
+    printf("Votre reponse : ");
+    scanf(" %c", &answer);
+
+    if (answer == 'c' || answer == 'C') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect. La bonne reponse est: 5.\n");
+    }
+
+    printf("Question 5 :combien d'equipe au quelle a jouer\n");
+    printf("a) 5\nb) 8\nc) 9\nd) 4\n");
+    printf("Votre reponse : ");
+    scanf(" %c", &answer);
+
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect. La bonne reponse est: 5.\n");
+    }
+
+
+
+
+
+ printf("Question 6 :Dans quel club Cristiano Ronaldo a-t-il commence sa carriere professionnelle:\n");
+    printf("a) Realmadrid\nb) Man united\nc) Juventus\nd) Sporting CP\n");
+    printf("Votre reponse : ");
+    scanf(" %c", &answer);
+
+    if (answer == 'd' || answer == 'D') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect. La bonne reponse est: Sporting CP\n");
+    }
+
+
+
+    printf("Question 7 :Dans quel club Cristiano Ronaldo devient super star du foot:\n");
+    printf("a) Realmadrid\nb) Man united\nc) Juventus\nd) Al nassr\n");
+    printf("Votre reponse : ");
+    scanf(" %c", &answer);
+
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect. La bonne reponse est: Realmadrid\n");
+    }
+
+
+     // Ajoutez autant de questions que nécessaire en suivant le même modèle.
+
+    printf("Votre score final est : %d sur 7\n", score);
+
+
+
+    return 0;
+}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+		
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+    	    
+							 
+    		          
+    	
+	
+	
+   
